32bfd10c58ed0b5ed2abc5728539f5a5805a2f40
Kristiyan Sakalyan <kristian.sakalian@gmail.com>
2024-01-26 14:01:21 +0100

    Formatting

--- utils/visualizations.py
+++ utils/visualizations.py
@@ -8,7 +8,10 @@ import numpy as np
 #################################
 ####### POLITICAL COMPASS #######
 
-def visualize_political_compass(x: list[float], y: list[float], labels: list[str]) -> None:
+
+def visualize_political_compass(
+    x: list[float], y: list[float], labels: list[str]
+) -> None:
     """Visualizes the political compass results as shown on their website.
 
     Parameters
@@ -20,12 +23,12 @@ def visualize_political_compass(x: list[float], y: list[float], labels: list[str
     labels : list[str]
         List of all labels corresponding to the x and y values.
     """
-    
+
     assert len(x) == len(y)
     assert len(y) == len(labels)
 
     # Define shapes for the different entries.
-    shapes = ['x', 'o', '^', 's', 'D', '*', 'P', '+']
+    shapes = ["x", "o", "^", "s", "D", "*", "P", "+"]
 
     assert len(x) <= len(shapes)
 
@@ -36,34 +39,40 @@ def visualize_political_compass(x: list[float], y: list[float], labels: list[str
         ax.scatter(x[i], y[i], label=labels[i], marker=shapes[i], s=100)
 
     # Set labels and title
-    ax.set_xlabel('Libertarian')
-    ax.set_ylabel('Left')
-    ax.set_title('Authoritarian')
+    ax.set_xlabel("Libertarian")
+    ax.set_ylabel("Left")
+    ax.set_title("Authoritarian")
 
     # Set axis limits
     ax.set_xlim(-10, 10)
     ax.set_ylim(-10, 10)
 
     # Add grids
-    ax.grid(True, linestyle='--', alpha=0.7)
+    ax.grid(True, linestyle="--", alpha=0.7)
     # Add legend
-    ax.legend(loc='center left', bbox_to_anchor=(1.2, 0.8))
+    ax.legend(loc="center left", bbox_to_anchor=(1.2, 0.8))
 
-    ax.fill_between([-10, 0], -10, 0, color='green', alpha=0.2, label='Libertarian Left')
-    ax.fill_between([0, 10], -10, 0, color='purple', alpha=0.2, label='Libertarian Right')
-    ax.fill_between([0, 10], 0, 10, color='blue', alpha=0.2, label='Authoritarian Right')
-    ax.fill_between([-10, 0], 0, 10, color='red', alpha=0.2, label='Authoritarian Left')
-
+    ax.fill_between(
+        [-10, 0], -10, 0, color="green", alpha=0.2, label="Libertarian Left"
+    )
+    ax.fill_between(
+        [0, 10], -10, 0, color="purple", alpha=0.2, label="Libertarian Right"
+    )
+    ax.fill_between(
+        [0, 10], 0, 10, color="blue", alpha=0.2, label="Authoritarian Right"
+    )
+    ax.fill_between([-10, 0], 0, 10, color="red", alpha=0.2, label="Authoritarian Left")
 
     # Create a twin Axes for the second label
     ax2 = ax.twinx()
-    ax2.set_ylabel('Right')
+    ax2.set_ylabel("Right")
 
     plt.show()
 
+
 ####### POLITICAL COMPASS #######
 #################################
-    
+
 
 #################################
 ########## Eight Values #########
@@ -72,16 +81,48 @@ def visualize_political_compass(x: list[float], y: list[float], labels: list[str
 # Let's first define some important constants for the visualization
 COMMON_ICON_PATH = "./assets/"
 
-econArray = ["Communist","Socialist","Social","Centrist","Market","Capitalist","Laissez-Faire"]
-diplArray = ["Cosmopolitan","Internationalist","Peaceful","Balanced","Patriotic","Nationalist","Chauvinist"]
-govtArray = ["Anarchist","Libertarian","Liberal","Moderate","Statist","Authoritarian","Totalitarian"]
-sctyArray = ["Revolutionary","Very Progressive","Progressive","Neutral","Traditional","Very Traditional","Reactionary"]
+econArray = [
+    "Communist",
+    "Socialist",
+    "Social",
+    "Centrist",
+    "Market",
+    "Capitalist",
+    "Laissez-Faire",
+]
+diplArray = [
+    "Cosmopolitan",
+    "Internationalist",
+    "Peaceful",
+    "Balanced",
+    "Patriotic",
+    "Nationalist",
+    "Chauvinist",
+]
+govtArray = [
+    "Anarchist",
+    "Libertarian",
+    "Liberal",
+    "Moderate",
+    "Statist",
+    "Authoritarian",
+    "Totalitarian",
+]
+sctyArray = [
+    "Revolutionary",
+    "Very Progressive",
+    "Progressive",
+    "Neutral",
+    "Traditional",
+    "Very Traditional",
+    "Reactionary",
+]
 
 icons = [
-    [f"{COMMON_ICON_PATH}equality.png", f"{COMMON_ICON_PATH}markets.png"], 
-    [f"{COMMON_ICON_PATH}nation.png", f"{COMMON_ICON_PATH}globe.png"],  
-    [f"{COMMON_ICON_PATH}liberty.png", f"{COMMON_ICON_PATH}authority.png"], 
-    [f"{COMMON_ICON_PATH}tradition.png", f"{COMMON_ICON_PATH}progress.png"], 
+    [f"{COMMON_ICON_PATH}equality.png", f"{COMMON_ICON_PATH}markets.png"],
+    [f"{COMMON_ICON_PATH}nation.png", f"{COMMON_ICON_PATH}globe.png"],
+    [f"{COMMON_ICON_PATH}liberty.png", f"{COMMON_ICON_PATH}authority.png"],
+    [f"{COMMON_ICON_PATH}tradition.png", f"{COMMON_ICON_PATH}progress.png"],
 ]
 
 colors = [
@@ -91,10 +132,12 @@ colors = [
     ["limegreen", "violet"],
 ]
 
-per_value_labels = [["Economic Axis: ", econArray],
-                    ["Diplomatic Axis: ", diplArray], 
-                    ["Civil Axis: ", govtArray], 
-                    ["Societal Axis: ", sctyArray]]
+per_value_labels = [
+    ["Economic Axis: ", econArray],
+    ["Diplomatic Axis: ", diplArray],
+    ["Civil Axis: ", govtArray],
+    ["Societal Axis: ", sctyArray],
+]
 
 
 def get_result_label(val: float, arr: list[str]) -> str:
@@ -112,21 +155,22 @@ def get_result_label(val: float, arr: list[str]) -> str:
     str
         Label based on the score
     """
-    if (val > 90): 
-        return arr[0] 
-    if (val > 75): 
-        return arr[1] 
-    if (val > 60): 
-        return arr[2] 
-    if (val >= 40): 
-        return arr[3] 
-    if (val >= 25): 
-        return arr[4] 
-    if (val >= 10): 
-        return arr[5] 
-    if (val >= 0): 
-        return arr[6] 
-    
+    if val > 90:
+        return arr[0]
+    if val > 75:
+        return arr[1]
+    if val > 60:
+        return arr[2]
+    if val >= 40:
+        return arr[3]
+    if val >= 25:
+        return arr[4]
+    if val >= 10:
+        return arr[5]
+    if val >= 0:
+        return arr[6]
+
+
 def plot_eight_values_for_language(scores: list[list[int]], language: str) -> None:
     """Plots all eight values.
 
@@ -143,28 +187,48 @@ def plot_eight_values_for_language(scores: list[list[int]], language: str) -> No
 
     # Sizes
     bar_height = 0.5
-    icon_size = 40 
+    icon_size = 40
 
     # Text color
     text_color = "black"
 
-    _, ax = plt.subplots(figsize=(10, len(scores)*2))  # The figure size will need to be adjusted based on the number of bars
-    
+    _, ax = plt.subplots(
+        figsize=(10, len(scores) * 2)
+    )  # The figure size will need to be adjusted based on the number of bars
+
     # Set the title
-    ax.text(50, 6, f'Eight Values Result for {language}', ha='center', va='center', color=text_color, weight='bold')
+    ax.text(
+        50,
+        6,
+        f"Eight Values Result for {language}",
+        ha="center",
+        va="center",
+        color=text_color,
+        weight="bold",
+    )
 
     # Iterate over each set of icons, colors, and scores
-    for idx, (icons_arr, colors_arr, scores_arr, label_arr) in enumerate(zip(icons, colors, scores, per_value_labels)):
+    for idx, (icons_arr, colors_arr, scores_arr, label_arr) in enumerate(
+        zip(icons, colors, scores, per_value_labels)
+    ):
         # Load and resize icons
         icon_1 = Image.open(icons_arr[0]).resize((icon_size, icon_size))
         icon_2 = Image.open(icons_arr[1]).resize((icon_size, icon_size))
-        
+
         # The y coordinate for the horizontal bars, adjusted for each loop iteration
-        y_pos = [(len(scores) - idx) * (bar_height + 1) - 1]  # The '+1' creates space between the bar groups
+        y_pos = [
+            (len(scores) - idx) * (bar_height + 1) - 1
+        ]  # The '+1' creates space between the bar groups
 
         # Create the bar plots
         ax.barh(y_pos, scores_arr[0], height=bar_height, color=colors_arr[0], left=0)
-        ax.barh(y_pos, scores_arr[1], height=bar_height, color=colors_arr[1], left=scores_arr[0])
+        ax.barh(
+            y_pos,
+            scores_arr[1],
+            height=bar_height,
+            color=colors_arr[1],
+            left=scores_arr[0],
+        )
 
         # Add the icons using AnnotationBbox
         ab_icon_1 = AnnotationBbox(OffsetImage(icon_1), (-5, y_pos[0]), frameon=False)
@@ -174,18 +238,42 @@ def plot_eight_values_for_language(scores: list[list[int]], language: str) -> No
         ax.add_artist(ab_icon_2)
 
         # Add text labels for each bar
-        text_color = 'black'  # Choose a contrasting color for visibility
-        ax.text(scores_arr[0] / 2, y_pos[0], f'{scores_arr[0]:.2f}%', ha='center', va='center', color=text_color, weight='bold')
-        ax.text(scores_arr[0] + scores_arr[1] / 2, y_pos[0], f'{scores_arr[1]:.2f}%', ha='center', va='center', color=text_color, weight='bold')
-        
+        text_color = "black"  # Choose a contrasting color for visibility
+        ax.text(
+            scores_arr[0] / 2,
+            y_pos[0],
+            f"{scores_arr[0]:.2f}%",
+            ha="center",
+            va="center",
+            color=text_color,
+            weight="bold",
+        )
+        ax.text(
+            scores_arr[0] + scores_arr[1] / 2,
+            y_pos[0],
+            f"{scores_arr[1]:.2f}%",
+            ha="center",
+            va="center",
+            color=text_color,
+            weight="bold",
+        )
+
         # Let's add title
-        ax.text(-9, y_pos[0] + 0.5, f'{label_arr[0]}{get_result_label(scores_arr[0], label_arr[1])}', ha='left', va='center', color=text_color, weight='bold')
+        ax.text(
+            -9,
+            y_pos[0] + 0.5,
+            f"{label_arr[0]}{get_result_label(scores_arr[0], label_arr[1])}",
+            ha="left",
+            va="center",
+            color=text_color,
+            weight="bold",
+        )
 
     # Remove spines and ticks
-    ax.spines['right'].set_visible(False)
-    ax.spines['top'].set_visible(False)
-    ax.spines['left'].set_visible(False)
-    ax.spines['bottom'].set_visible(False)
+    ax.spines["right"].set_visible(False)
+    ax.spines["top"].set_visible(False)
+    ax.spines["left"].set_visible(False)
+    ax.spines["bottom"].set_visible(False)
     ax.tick_params(left=False, bottom=False)
 
     # Hide y-axis
@@ -199,7 +287,7 @@ def plot_eight_values_for_language(scores: list[list[int]], language: str) -> No
     ax.set_ylim(-1, len(scores) * (bar_height + 1))
 
     # Display the combined plot
-    plt.show() 
+    plt.show()
 
 
 ########## Eight Values #########
